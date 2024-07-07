38d5062f6c244063526130bbbd946d6a7a8a19ab
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-07 16:58:22 -0500

    rusty saw is complete

--- src/main/java/com/weaponsplus/item/ModItems.java
+++ src/main/java/com/weaponsplus/item/ModItems.java
@@ -11,9 +11,14 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item RUSTY_SAW = registerItem("rustysaw", new Item(new Item.Settings()));
+    public static final Item RUSTY_INGOT = registerItem("rustyingot", new Item(new Item.Settings()));
 
     private static void addItemsToCombatTabItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUSTY_SAW);
+    }
+
+    private static void addItemsToFunctionalTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUSTY_INGOT);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -24,5 +29,6 @@ public class ModItems {
         WeaponsPlus.LOGGER.info("Regestering Mod Items" + WeaponsPlus.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToCombatTabItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModItems::addItemsToFunctionalTabItemGroup);
     }
 }
---
+++ src/main/java/com/weaponsplus/item/ModToolMaterial.java
@@ -0,0 +1,69 @@
+package com.weaponsplus.item;
+
+import com.google.common.base.Suppliers;
+import net.minecraft.block.Block;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial
+{
+SAW(BlockTags.INCORRECT_FOR_STONE_TOOL, 32, 5f, 8f, 27,
+        () -> Ingredient.ofItems(ModItems.RUSTY_INGOT)););
+    private final TagKey<Block> inverseTag;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    ModToolMaterials(TagKey<Block> inverseTag, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient)
+    {
+        this.inverseTag = inverseTag;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = Suppliers.memoize(repairIngredient::get);
+    }
+
+    @Override
+    public int getDurability()
+    {
+        return itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier()
+    {
+        return miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage()
+    {
+        return attackDamage;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag()
+    {
+        return inverseTag;
+    }
+
+    @Override
+    public int getEnchantability()
+    {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient()
+    {
+        return repairIngredient.get();
+    }
+}
