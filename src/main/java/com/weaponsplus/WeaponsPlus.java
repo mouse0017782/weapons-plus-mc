49c6e4087a043e8fd4723c50d9fb8e9991a90854
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-07 14:04:28 -0500

    created way to add items to creative tab

--- src/main/java/com/weaponsplus/WeaponsPlus.java
+++ src/main/java/com/weaponsplus/WeaponsPlus.java
@@ -1,5 +1,6 @@
 package com.weaponsplus;
 
+import com.weaponsplus.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,10 +12,8 @@ public class WeaponsPlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/com/weaponsplus/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.weaponsplus.item;
+
+import com.weaponsplus.WeaponsPlus;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item RUSTY_SAW = registerItem("rustysaw", new Item(new Item.Settings()));
+
+    private static void addItemsToCombatTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUSTY_SAW);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(WeaponsPlus.MOD_ID, name), item);
+    }
+
+    public static void  registerModItems() {
+        WeaponsPlus.LOGGER.info("Regestering Mod Items" + WeaponsPlus.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToCombatTabItemGroup);
+    }
+}
