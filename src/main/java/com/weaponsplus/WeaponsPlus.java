7cde0c99662f6936e49e4d7f06d8c1543ac1b5eb
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-16 20:44:55 -0500

    items and jazz

--- src/main/java/com/weaponsplus/WeaponsPlus.java
+++ src/main/java/com/weaponsplus/WeaponsPlus.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WeaponsPlus implements ModInitializer {
+	public static final String MOD_ID = "weaponsplus";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -13,6 +14,7 @@ public class WeaponsPlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
