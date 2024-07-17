66e8d7bfcffead608920d7ef64d9b98140518fa6
mouse0017782 <81055997+mouse0017782@users.noreply.github.com>
2024-07-16 20:10:36 -0500

    setup

---
+++ src/main/java/com/weaponsplus/WeaponsPlusClient.java
@@ -0,0 +1,11 @@
+package com.weaponsplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponsPlusClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
